1f5252fb7942e5fbcaf7279e77ec78dc11286873
yingweijun <2973971708@qq.com>
2023-12-15 11:41:32 +0800

    项目初始化结构

---
+++ product/src/test/java/ywj/ywj_product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package ywj.ywj_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ware/src/main/java/ywj/ywj_ware/controller/Test.java
@@ -0,0 +1,14 @@
+package ywj.ywj_ware.controller;
+
+/**
+ * ClassName: Test
+ * Package: ywj.ywj_ware.controller
+ * Description:
+ *
+ * @Author 应伟军
+ * @Create 2023/12/14 23:20
+ * @Version 1.0
+ */
+public class Test {
+
+}
